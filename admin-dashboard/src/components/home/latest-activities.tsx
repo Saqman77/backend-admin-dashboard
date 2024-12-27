3f4cde1b85a0341e45214897f2c2a6dab549e1cb
Saqman77 <saqlainhaider77@gmail.com>
2024-12-27 13:17:21 +0500

    setup

--- admin-dashboard/src/components/home/latest-activities.tsx
+++ admin-dashboard/src/components/home/latest-activities.tsx
@@ -1,8 +1,30 @@
+import { UnorderedListOutlined } from '@ant-design/icons'
+import { Card } from 'antd'
 import React from 'react'
+import { Text } from '../text'
 
 const LatestActivities = () => {
   return (
-    <div>LatestActivities</div>
+    <Card
+    headStyle={{ padding: '16px'}}
+    bodyStyle={{ padding: '0 1rem'}}
+    title={
+    (<div
+    style={
+        {
+            display:'flex',
+            alignItems: 'center',
+            gap: '8px'
+        }}
+    >
+        <UnorderedListOutlined />
+        <Text size='sm' style={{ marginLeft: '0.5rem'}}>
+            Latest Activities
+        </Text>
+    </div>)}
+    >
+        LatestActivities
+    </Card>
   )
 }
 
